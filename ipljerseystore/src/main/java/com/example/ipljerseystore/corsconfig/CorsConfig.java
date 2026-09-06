655d78bb66c209c8cbd72ec29e5ca40e7d26e617
kjs-3 <justaddsj@gmail.com>
2026-09-06 16:22:30 +0530

    Update CorsConfig.java added config class

--- ipljerseystore/src/main/java/com/example/ipljerseystore/corsconfig/CorsConfig.java
+++ ipljerseystore/src/main/java/com/example/ipljerseystore/corsconfig/CorsConfig.java
@@ -1,17 +1,19 @@
-//package com.example.ipljerseystore.corsconfig;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry){
-//        registry.addMapping("/api/**")
-//        .allowedOrigins("http://localhost:5173")
-//                .allowedMethods("GET","POST","PUT","DELETE")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
-//}
+package com.example.ipljerseystore.corsconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+   @Override
+   public void addCorsMappings(CorsRegistry registry){
+       registry.addMapping("/api/**")
+       .allowedOrigins("http://localhost:5173","https://ipl-store-frontend-c1w5kdt9f-kjs-3s-projects.vercel.app","https://*.vercel.app")
+               .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+               .allowedHeaders("*")
+               .allowCredentials(true)
+         .exposedHeaders("*");
+             
+   }
+}
