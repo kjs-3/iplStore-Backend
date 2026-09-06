c8251ffb26ce4c3a02c41d0ca1244a34dc643a7c
kjs-3 <justaddsj@gmail.com>
2026-09-06 21:06:17 +0530

    Update Users.java rearranged orders

--- ipljerseystore/src/main/java/com/example/ipljerseystore/userentity/Users.java
+++ ipljerseystore/src/main/java/com/example/ipljerseystore/userentity/Users.java
@@ -15,6 +15,28 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Table(name="users")
 public class Users {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private long userId;
+    @Column(name="user_name")
+    private String userName;
+    @Column(name="user_email")
+    private String userEmail;
+    @Column(name="user_password")
+    private String userPassword;
+    @Column(name="user_phonenumber")
+    private long userPhno;
+    @Enumerated(EnumType.STRING)
+    @Column(name="user_role")
+    @Builder.Default
+    private Role role=Role.user;
+    @Column(name = "created_at")
+    private LocalDateTime ldc=LocalDateTime.now();
+    public enum Role{
+        user,admin
+    }
     public long getUserId() {
         return userId;
     }
@@ -71,25 +93,4 @@ public class Users {
         this.ldc = ldc;
     }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="user_id")
-    private long userId;
-    @Column(name="user_name")
-    private String userName;
-    @Column(name="user_email")
-    private String userEmail;
-    @Column(name="user_password")
-    private String userPassword;
-    @Column(name="user_phonenumber")
-    private long userPhno;
-    @Enumerated(EnumType.STRING)
-    @Column(name="user_role")
-    @Builder.Default
-    private Role role=Role.user;
-    @Column(name = "created_at")
-    private LocalDateTime ldc=LocalDateTime.now();
-    public enum Role{
-        user,admin
-    }
 }
