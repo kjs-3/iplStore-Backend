2fcf44b0fb2938002a176694db85d5c542122c75
kjs-3 <justaddsj@gmail.com>
2026-09-06 21:04:44 +0530

    Update Products.java rearranged the order

--- ipljerseystore/src/main/java/com/example/ipljerseystore/productentity/Products.java
+++ ipljerseystore/src/main/java/com/example/ipljerseystore/productentity/Products.java
@@ -18,6 +18,33 @@ import java.util.List;
 @NoArgsConstructor
 @Table(name="products")
 public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="product_id")
+    private long productId;
+    @Column(name="product_name")
+    private String productName;
+    @Column(name="product_description")
+    private String productDescription;
+    @Column(name="product_image")
+    private String productImage;
+    @Column(name="product_price")
+    private double productPrice;
+    @Column(name="product_size")
+    private String productSize;
+    @Column(name="teamname")
+    private String teamName;
+    @Column(name="product_stock")
+    private int productStock;
+    @Column(name="created_at")
+    LocalDateTime createdat=LocalDateTime.now();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="category_id")
+    Category category;
+    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL,mappedBy="products")
+    List<Cart> cartItems;
+    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL,mappedBy="products")
+    List<OrderItems> orderItems;
     public long getProductId() {
         return productId;
     }
@@ -114,31 +141,5 @@ public class Products {
         this.orderItems = orderItems;
     }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="product_id")
-    private long productId;
-    @Column(name="product_name")
-    private String productName;
-    @Column(name="product_description")
-    private String productDescription;
-    @Column(name="product_image")
-    private String productImage;
-    @Column(name="product_price")
-    private double productPrice;
-    @Column(name="product_size")
-    private String productSize;
-    @Column(name="teamname")
-    private String teamName;
-    @Column(name="product_stock")
-    private int productStock;
-    @Column(name="created_at")
-    LocalDateTime createdat=LocalDateTime.now();
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="category_id")
-    Category category;
-    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL,mappedBy="products")
-    List<Cart> cartItems;
-    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL,mappedBy="products")
-    List<OrderItems> orderItems;
+    
 }
