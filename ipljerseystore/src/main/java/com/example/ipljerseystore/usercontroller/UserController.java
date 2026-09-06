bd11b3022aedfe6d2c870a1000377a1b7fbca174
kjs-3 <justaddsj@gmail.com>
2026-09-06 16:23:26 +0530

    Update UserController.java commented crossorigin

--- ipljerseystore/src/main/java/com/example/ipljerseystore/usercontroller/UserController.java
+++ ipljerseystore/src/main/java/com/example/ipljerseystore/usercontroller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins ="http://localhost:5173","https://ipl-store-frontend-c1w5kdt9f-kjs-3s-projects.vercel.app","https://*.vercel.app",allowCredentials = "true")
+// @CrossOrigin(origins ="http://localhost:5173","https://ipl-store-frontend-c1w5kdt9f-kjs-3s-projects.vercel.app","https://*.vercel.app",allowCredentials = "true")
 public class UserController {
     @Autowired
     private UserService us;
