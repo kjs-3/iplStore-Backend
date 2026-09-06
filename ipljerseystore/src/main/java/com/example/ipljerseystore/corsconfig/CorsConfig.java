72e3ca561b25e2de64014a48891603337480172a
kjs-3 <justaddsj@gmail.com>
2026-09-06 16:44:04 +0530

    Update CorsConfig.java modified entire Cors config

--- ipljerseystore/src/main/java/com/example/ipljerseystore/corsconfig/CorsConfig.java
+++ ipljerseystore/src/main/java/com/example/ipljerseystore/corsconfig/CorsConfig.java
@@ -1,15 +1,21 @@
 package com.example.ipljerseystore.corsconfig;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig  {
+   @Bean{
+      public WebMvcConfigurer corsConfigurer(){
+         return new WebMvcConfigurer(){
+            
    @Override
    public void addCorsMappings(CorsRegistry registry){
        registry.addMapping("/**")
-       .allowedOrigins("http://localhost:5173","https://ipl-store-frontend-c1w5kdt9f-kjs-3s-projects.vercel.app","https://*.vercel.app")
+       .allowedOrigins("http://localhost:5173",
+                       "https://ipl-store-frontend-c1w5kdt9f-kjs-3s-projects.vercel.app",
+                       "https://*.vercel.app")
                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                .allowedHeaders("*")
                .allowCredentials(true)
