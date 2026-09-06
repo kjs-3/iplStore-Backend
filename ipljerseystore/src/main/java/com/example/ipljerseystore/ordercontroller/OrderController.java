588352692980184f327430a2c3616b026d215c43
kjs-3 <justaddsj@gmail.com>
2026-09-06 16:11:21 +0530

    Update OrderController.java updated deployed url

--- ipljerseystore/src/main/java/com/example/ipljerseystore/ordercontroller/OrderController.java
+++ ipljerseystore/src/main/java/com/example/ipljerseystore/ordercontroller/OrderController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
-@CrossOrigin(origins = "http://localhost:5173",allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:5173","https://ipl-store-frontend-c1w5kdt9f-kjs-3s-projects.vercel.app","https://*.vercel.app",allowCredentials = "true")
 public class OrderController {
     @Autowired
     private OrderService orderService;
