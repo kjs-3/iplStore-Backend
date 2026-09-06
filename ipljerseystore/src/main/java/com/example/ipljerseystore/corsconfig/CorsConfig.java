a1a4216f7780877478703f9a0b4d5d87e845b7d7
kjs-3 <justaddsj@gmail.com>
2026-09-06 19:36:50 +0530

    Update CorsConfig.java added ResourceHandler

--- ipljerseystore/src/main/java/com/example/ipljerseystore/corsconfig/CorsConfig.java
+++ ipljerseystore/src/main/java/com/example/ipljerseystore/corsconfig/CorsConfig.java
@@ -3,6 +3,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 public class CorsConfig  {
@@ -22,6 +23,11 @@ public class CorsConfig  {
          .exposedHeaders("*");
              
    }
+            @Override
+            public void addResourceHandlers(ResourceHandlersRegistry registry){
+               registry.addResourceHandler("images/**)
+                  .addResourceLocations("classpath:/static/images/");
+            }
          };
 }
 }
