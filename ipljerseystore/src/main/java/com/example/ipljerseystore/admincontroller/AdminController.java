4d241dbc38c3381148e4fdd141a864ebe3b0fec4
kjs-3 <justaddsj@gmail.com>
2026-09-07 00:17:55 +0530

    Update AdminController.java added restcontroller

--- ipljerseystore/src/main/java/com/example/ipljerseystore/admincontroller/AdminController.java
+++ ipljerseystore/src/main/java/com/example/ipljerseystore/admincontroller/AdminController.java
@@ -13,8 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.*;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RestController;
+ import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
     @RequestMapping("/api/admin")
